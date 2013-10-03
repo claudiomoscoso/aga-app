6a86bdd6782ae56550b5adca00d89085f480144e
claudio.moscoso@gmail.com <claudio.moscoso@gmail.com@b687ddb2-1db7-f4f9-b36e-c76fe5e98f2f>
2013-10-03 03:28:05 +0000

    Se desarrollan metodos de usuario.

--- trunk/src/clinica/beans/Consulta.java
+++ trunk/src/clinica/beans/Consulta.java
@@ -4,77 +4,85 @@ import java.util.Date;
 import java.util.List;
 
 public class Consulta {
+	private Integer codigo = null;
+	private Date fecha;
+	private Paciente paciente;
+	private Medico medico;
+	private String diagnostico;
+	private String tratamiento;
+	// private String observaciones;
+	private TipoConsulta tipoConsulta;
+	private List<Examen> examenes;
 
-    private Date fecha;
-    private Paciente paciente;
-    private Medico medico;
-    private String diagnostico;
-    private String tratamiento;
-    private String observaciones;
-    private TipoConsulta tipoConsulta;
-    private List<Examen> examenes;
+	public Date getFecha() {
+		return fecha;
+	}
 
-    public Date getFecha() {
-        return fecha;
-    }
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
 
-    public void setFecha(Date fecha) {
-        this.fecha = fecha;
-    }
+	public Paciente getPaciente() {
+		return paciente;
+	}
 
-    public Paciente getPaciente() {
-        return paciente;
-    }
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
 
-    public void setPaciente(Paciente paciente) {
-        this.paciente = paciente;
-    }
+	public Medico getMedico() {
+		return medico;
+	}
 
-    public Medico getMedico() {
-        return medico;
-    }
+	public void setMedico(Medico medico) {
+		this.medico = medico;
+	}
 
-    public void setMedico(Medico medico) {
-        this.medico = medico;
-    }
+	public String getDiagnostico() {
+		return diagnostico;
+	}
 
-    public String getDiagnostico() {
-        return diagnostico;
-    }
+	public void setDiagnostico(String diagnostico) {
+		this.diagnostico = diagnostico;
+	}
 
-    public void setDiagnostico(String diagnostico) {
-        this.diagnostico = diagnostico;
-    }
+	public String getTratamiento() {
+		return tratamiento;
+	}
 
-    public String getTratamiento() {
-        return tratamiento;
-    }
+	public void setTratamiento(String tratamiento) {
+		this.tratamiento = tratamiento;
+	}
 
-    public void setTratamiento(String tratamiento) {
-        this.tratamiento = tratamiento;
-    }
+	// public String getObservaciones() {
+	// return observaciones;
+	// }
+	//
+	// public void setObservaciones(String observaciones) {
+	// this.observaciones = observaciones;
+	// }
 
-    public String getObservaciones() {
-        return observaciones;
-    }
+	public TipoConsulta getTipoConsulta() {
+		return tipoConsulta;
+	}
 
-    public void setObservaciones(String observaciones) {
-        this.observaciones = observaciones;
-    }
+	public void setTipoConsulta(TipoConsulta tipoConsulta) {
+		this.tipoConsulta = tipoConsulta;
+	}
 
-    public TipoConsulta getTipoConsulta() {
-        return tipoConsulta;
-    }
+	public List<Examen> getExamenes() {
+		return examenes;
+	}
 
-    public void setTipoConsulta(TipoConsulta tipoConsulta) {
-        this.tipoConsulta = tipoConsulta;
-    }
+	public void setExamenes(List<Examen> examenes) {
+		this.examenes = examenes;
+	}
 
-    public List<Examen> getExamenes() {
-        return examenes;
-    }
+	public Integer getCodigo() {
+		return codigo;
+	}
 
-    public void setExamenes(List<Examen> examenes) {
-        this.examenes = examenes;
-    }
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
 }
