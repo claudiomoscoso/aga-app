60696e961138a8f71ee383c338759de9eccc02a6
claudio.moscoso@gmail.com <claudio.moscoso@gmail.com@b687ddb2-1db7-f4f9-b36e-c76fe5e98f2f>
2013-10-06 22:44:29 +0000

    Desarrollo de clases de prueba para clinica.

--- trunk/src/clinica/acciones/AccionesConsulta.java
+++ trunk/src/clinica/acciones/AccionesConsulta.java
@@ -3,7 +3,6 @@ package clinica.acciones;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
 
 import clinica.beans.Consulta;
 import clinica.util.Conexion;
@@ -12,26 +11,42 @@ public class AccionesConsulta {
 	public void confirmar(Consulta c) throws Exception {
 		Connection conn = Conexion.obtenerConexion();
 
-		String sql = "insert into consultas_medicas ( rut_paciente, rut_medico, ";
+		Integer lastKey = getLastCode(conn, "consultas_medicas");
+		lastKey++;
+		c.setCodigo(lastKey);
+
+		String sql = "insert into consultas_medicas (codigo, rut_paciente, rut_medico, ";
 		sql += "fecha_consulta, diagnostico, tratamiento, tipo_consulta) ";
-		sql += "values (?, ?, ?, ?, ?, ?)";
+		sql += "values (?, ?, ?, ?, ?, ?, ?)";
 
-		PreparedStatement s = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+		PreparedStatement s = conn.prepareStatement(sql);
+		// Statement s = conn.createStatement( );
 
-		s.setString(1, c.getPaciente().getRut());
-		s.setString(2, c.getMedico().getRut());
-		s.setDate(3, (java.sql.Date) c.getFecha());
-		s.setString(4, c.getDiagnostico());
-		s.setString(5, c.getTratamiento());
-		s.setString(6, "" + c.getTipoConsulta().toString().charAt(0));
+		// preparedStatement = conn.prepareStatement(sql,
+		// Statement.RETURN_GENERATED_KEYS);
+		s.setInt(1, c.getCodigo());
+		s.setString(2, c.getPaciente().getRut());
+		s.setString(3, c.getMedico().getRut());
+		s.setDate(4, (java.sql.Date) c.getFecha());
+		s.setString(5, c.getDiagnostico());
+		s.setString(6, c.getTratamiento());
+		s.setString(7, "" + c.getTipoConsulta().toString().charAt(0));
 
 		s.executeUpdate();
 
-		ResultSet generatedKeys = s.getGeneratedKeys();
-		if (generatedKeys.next()) {
-			c.setCodigo(((Long) generatedKeys.getLong(1)).intValue());
+		Conexion.cerrarTodo(null, s, conn);
+	}
+
+	private Integer getLastCode(Connection conn, String tabla) throws Exception {
+		Integer out = -1;
+		String sql = "select max(codigo) from " + tabla;
+		PreparedStatement s = conn.prepareStatement(sql);
+
+		ResultSet rs = s.executeQuery();
+		if (rs.next()) {
+			out = rs.getInt(1);
 		}
 
-		Conexion.cerrarTodo(null, s, conn);
+		return out;
 	}
 }
